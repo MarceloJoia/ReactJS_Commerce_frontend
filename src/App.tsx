2b2e5cf4a28e83b7fcea8560a0919e7ea52bbbc2
Marcelo Joia <sitejoia@hotmail.com>
2023-06-22 10:53:35 -0300

    componente HeaderClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-
-import computerImg from './assets/computer.png';
-import cartImg from './assets/cart.svg';
 import './App.css';
+import computerImg from './assets/computer.png';
+import HeaderClient from './components/HeaderClient';
 
 function App() {
 
   return (
     <>
-      <header className="dsc-header-client">
-      <nav className="dsc-container">
-        <h1>DSCommerce</h1>
-        <div className="dsc-navbar-right">
-          <div className="dsc-menu-items-container">
-            <div className="dsc-menu-item">
-              <img src={cartImg} alt="Carrinho de compras" />
-            </div>
-          </div>
-          <a href="#">Entrar</a>
-        </div>
-      </nav>
-    </header>
+    <HeaderClient/>
+      
     <main>
       <section id="product-details-section" className="dsc-container">
         <div className="dsc-card dsc-mb20">
@@ -48,6 +36,7 @@ function App() {
             </div>
           </div>
         </div>
+
         <div className="dsc-btn-page-container">
             <div className="dsc-btn dsc-btn-blue">
                 Comprar
@@ -56,6 +45,7 @@ function App() {
                 Início
             </div>
         </div>
+        
       </section>
     </main>
     </>
