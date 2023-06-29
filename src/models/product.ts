0aaaa364175646eded92108cc0d0b8252f28d805
Marcelo Joia <sitejoia@hotmail.com>
2023-06-28 23:14:00 -0300

    Props productDetails

---
+++ src/models/product.ts
@@ -0,0 +1,12 @@
+import { CategoryDTO } from "./Category"
+
+export type ProductDTO = {
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    imgUrl: string,
+    categories: CategoryDTO[
+        
+    ],
+}
--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -5,6 +5,26 @@ import ProductDetailsCard from '../../components/ProductDetailsCard';
 import ButtonPrimary from '../../components/ButtonPrimary';
 import ButtonInverse from '../../components/ButtonInverse';
 
+import { ProductDTO } from '../../models/product';
+
+//=> Criar uma constante
+const product: ProductDTO = {
+  id: 2,
+  name: 'Smart TV',
+  description: 'TV Smart de 60" 4K HD 512GB bluetooth',
+  imgUrl: 'https://raw.githubusercontent.com/MarceloJoia/dscatalog-resources/master/backend/img/1-big.jpg',
+  price: 2952.99,
+  categories: [
+    {
+      id: 2,
+      nome: "Eletrônicos",
+    },
+    {
+      id: 3,
+      nome: "Computadores",
+    },
+  ]
+};
 
 export default function ProductDetails() {
 
@@ -13,7 +33,7 @@ export default function ProductDetails() {
       <HeaderClient />
       <main>
         <section id="product-details-section" className="dsc-container">
-          <ProductDetailsCard />
+          <ProductDetailsCard product={product} />
           <div className="dsc-btn-page-container">
             <ButtonPrimary />
             <ButtonInverse />
